2a57307ac5954b6b00dc668729674f5507ffbe59
ミシャ <47413092+MihailsKuzmins@users.noreply.github.com>
2021-12-26 22:22:36 +0200

    add docs

--- src/MyNihongo.KanjiVG.Animator/Services/IKanjiAnimatorService.cs
+++ src/MyNihongo.KanjiVG.Animator/Services/IKanjiAnimatorService.cs
@@ -2,5 +2,12 @@
 
 public interface IKanjiAnimatorService
 {
+	/// <summary>
+	/// Generates the animated KanjiVG string
+	/// </summary>
+	/// <param name="text">Original KanjiVG text</param>
+	/// <param name="fileName">Name of the KanjiVG file</param>
+	/// <param name="svgParams">Parameters for creating the animation</param>
+	/// <returns>Animated KanjiVG text</returns>
 	string Generate(string text, string fileName, SvgParams svgParams);
 }
